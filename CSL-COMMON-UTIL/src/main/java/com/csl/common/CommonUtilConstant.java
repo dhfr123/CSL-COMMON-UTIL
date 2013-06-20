9ca1e739fc62175eb004cae134c58c2c39fd0c08
AdityaSatrioNugroho <AdityaSatrioNugroho@impulse.mshome.net>
2013-06-20 18:49:38 +0700

    fix typo

--- CSL-COMMON-UTIL/src/main/java/com/csl/common/CommonUtilConstant.java
+++ CSL-COMMON-UTIL/src/main/java/com/csl/common/CommonUtilConstant.java
@@ -12,7 +12,7 @@ public class CommonUtilConstant {
 
     public static final String NUMBER_FORMAT_TYPE_CURRENCY = "currency";
     public static final String NUMBER_FORMAT_TYPE_NUMBER = "number";
-    public static final String NUMBER_FORMAT_TYPE_PERCENT = "persent";
+    public static final String NUMBER_FORMAT_TYPE_PERCENT = "percent";
     public static final String MILLISECOND = "millisecond";
     public static final String SECOND = "second";
     public static final String MINUTES = "minutes";
